93a73bc9efc8b4b68affadd6da3c2bdf12771455
thethtun <thethtun4040@gmail.com>
2020-06-06 22:33:40 +0630

    Add more titles

---
+++ data/prepositions.js
@@ -0,0 +1,142 @@
+export default [
+    {
+        question: "He is living in Paris",
+        answer: "á",
+        highlight: "in",
+    },
+    {
+        question: "I bought this coat for $200",
+        answer: "pour",
+        highlight: "for",
+    },
+    {
+        question: "It’s hard without money",
+        answer: "sans",
+        highlight: "without",
+    },
+    {
+        question: "Mg Mg digs with a shovel.",
+        answer: "avec",
+        highlight: "with",
+    },
+    {
+        question: "They go to school from 8:30 a.m. to 3:30 p.m.",
+        answer: "de",
+        highlight: "from",
+    },
+    {
+        question: "The bakery is close to the hotel.",
+        answer: "á côté de",
+        highlight: "close to",
+    },
+    {
+        question: "I'm next to the swimming pool.",
+        answer: "á côté de",
+        highlight: "next to",
+    },
+    {
+        question: "They are sitting behind",
+        answer: "derrière",
+        highlight: "behind",
+    },
+    {
+        question: "They are sitting in front",
+        answer: "devant",
+        highlight: "in front",
+    },
+    {
+        question: "Go on holidays with your couple or children and save up to 20 on your curise during all the season.",
+        answer: "au long de",
+        highlight: "during",
+    },
+    {
+        question: "Declining buffalo herds and disease put many nations on the verge of extinction.",
+        answer: "au bord de",
+        highlight: "on the verge of",
+    },
+    {
+        question: "Feel, relax, enjoy the best products at the center of Yangon.",
+        answer: "au centre de",
+        highlight: "at the center of",
+    },
+    {
+        question: "This is going to happen in the middle of 2009",
+        answer: "au milieu de",
+        highlight: "in the middle of",
+    },
+    {
+        question: "He lives at the top of this hill.",
+        answer: "au sommet de",
+        highlight: "at the top of",
+    },
+    {
+        question: "Your shoes are at the bottom of your closet.",
+        answer: "au fond de",
+        highlight: "at the bottom of",
+    },
+    {
+        question: "at the end of three days, the situation stabilized.",
+        answer: "au bout de",
+        highlight: "at the end of",
+    },
+    {
+        question: "Prices available on request.",
+        answer: "sur",
+        highlight: "on",
+    },
+    {
+        question: "Please check the question written above",
+        answer: "dessus",
+        highlight: "above",
+    },
+    {
+        question: "Children are under parent’s protections.",
+        answer: "sous",
+        highlight: "under",
+    },
+    {
+        question: "This price is listed below",
+        answer: "dessous",
+        highlight: "below",
+    },
+    {
+        question: "We expect to finish the project in a year",
+        answer: "dans",
+        highlight: "in",
+    },
+    {
+        question: "Nothing can be found inside the building",
+        answer: "dedans",
+        highlight: "inside",
+    },
+    {
+        question: "The most populated city in France is Paris.",
+        answer: "en",
+        highlight: "in",
+    },
+    {
+        question: "Myanmar gained independency in 1945",
+        answer: "en",
+        highlight: "in",
+    },
+    {
+        question: "Here, take that and drop it by my house",
+        answer: "chez moi",
+        highlight: "my house",
+    },
+    {
+        question: "Mitchell's out of his mind right now.",
+        answer: "hors de",
+        highlight: "out of",
+    },
+    {
+        question: "Dr. Cranston's waiting outside for you.",
+        answer: "dehors",
+        highlight: "outside",
+    },
+    {
+        question: " Jane parked her car on the side of the road.",
+        answer: "au bord de",
+        highlight: "on the side of",
+    },
+]
---
+++ data/q_type.js
@@ -0,0 +1,6 @@
+export default [
+    {
+        name : "Prepositions",
+        key: "prepositions",
+    }
+]
